92b42830ee6465b5a21a844e82de7c3b91b16193
icpmtech <mourao.martins@gmail.com>
2015-09-08 13:10:53 +0100

    tetse

--- WebApplication1/Models/Model_3D.cs
+++ WebApplication1/Models/Model_3D.cs
@@ -5,12 +5,25 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
-    public class Model_3D
+    public class Model_Base
     {
         public int id { get; set; }
         public int WorkFlowState { get; set; }
         public string Name { get; set; }
-        public string OriginalFile { get; set; }
-        
+        public string TypeFile { get; set; }
+       
+    }
+    public class Model_3D : Model_Base
+    {
+       
+        public byte[] OriginalModel3D { get; set; }
+
+    }
+    public class Model_2D : Model_Base
+    {
+
+        public byte[] OriginalModel2D { get; set; }
+      
+
     }
 }
